f0b090ed378e8053e72504585476bf2dfc42e4ee
mikemills254 <mikemills930@gmail.com>
2022-09-12 08:16:40 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-putchar.c
+ * Author Mike Mills
+ */
+
+#include <stdio.h>
+/**
+ * main: Program that prints _putchar followed by new line
+ *
+ * Return Always 0
+ */
+int main(void)
+{
+	printf("_putchar");
+
+	return ('\n');
+}
