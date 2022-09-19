d65c83aff87d32aabfa8d53e629ee7bfd3472c11
mikemills254 <mikemills930@gmail.com>
2022-09-19 13:24:17 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 /*
  * File reset_to_98.c
  * Author Mike Mills
- */ 
-
+ */
 #include "holberton.h"
 
 /*
- * reset_to_98: Resets the value of n
- *
+ * reset_to_98: Takes a pointer to an int and updates
+ *               the value it points to to 98.
  * @n: Pointer to an int
  */
 
