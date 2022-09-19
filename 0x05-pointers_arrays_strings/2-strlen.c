094eb16ea9d5a00c39886c8428845883fc74a58d
mikemills254 <mikemills930@gmail.com>
2022-09-19 13:57:53 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,14 +6,14 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * _strlen: Prints the length of a string
+ * _strlen - Prints the length of a string
  * @s: The string to be measured length
  */
 size_t _strlen(const char *s)
 {
 	size_t length = 0;
 
-	while(*s++)
+	while (*s++)
 		length++;
 
 	return (length);
