dd068b36ee86de621ca432639717a470be23238c
mikemills254 <mikemills930@gmail.com>
2022-09-19 13:55:43 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/*
+ * File: 2-strlen.c
+ *
+ * Auth: Mike Mills
+ */
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen: Prints the length of a string
+ * @s: The string to be measured length
+ */
+size_t _strlen(const char *s)
+{
+	size_t length = 0;
+
+	while(*s++)
+		length++;
+
+	return (length);
+}
