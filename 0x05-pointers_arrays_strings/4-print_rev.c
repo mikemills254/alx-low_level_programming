a7e584cfad601e76ec60e1950704758518f38881
mikemills254 <mikemills930@gmail.com>
2022-09-19 19:02:47 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+/*
+ * File: 4-prints_rev.c
+ *
+ * Auth: Mike Mills
+ */
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_rev - Prints strings in reverse 
+ *
+ * @s: The string to be printed
+ */
+void print_rev(char *s)
+{
+	int len = 0, index;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		putchar(s[index]);
+
+	putchar('\n');
+}
