47f3c1b1907ffbe70afc267d5cc188497ac079f9
mikemills254 <mikemills930@gmail.com>
2022-09-19 19:55:12 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+/*
+ * File: 8-print_array.c
+ * Auth: Mike Mills
+ */
+#include "holberton.h"
+#include <stdio.h>
+/*
+ * print_array - Prints elements of an array
+ * @a: The array of integers
+ * @n: The numner of elements to be printed
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+		
+		if (index == n - 1)
+			continue;
+
+		printf(", ");
+	}
+
+	printf("\n");
+}
