c7b82e002feed2d6515b272bc8c588e27d4029f8
mikemills254 <mikemills930@gmail.com>
2022-09-19 19:29:11 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+/*
+ * File: 6-puts2.c
+ *
+ * Auth: mike mills
+ */
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts2 - prints every character of a string followes by a new line
+ * @str: The string containing characters
+ */
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index +=2)
+		putchar(str[index]);
+
+	putchar('\n');
+}
