49e70172676ff8f09e152a00f590b2d7eba49270
mikemills254 <mikemills930@gmail.com>
2022-09-19 19:57:16 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,7 +4,7 @@
  */
 #include "holberton.h"
 #include <stdio.h>
-/*
+/**
  * print_array - Prints elements of an array
  * @a: The array of integers
  * @n: The numner of elements to be printed
@@ -16,7 +16,7 @@ void print_array(int *a, int n)
 	for (index = 0; index < n; index++)
 	{
 		printf("%d", a[index]);
-		
+
 		if (index == n - 1)
 			continue;
 
