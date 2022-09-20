aa6585c3469b493c9ba4018dcef6a6582c84e6f7
mikemills254 <mikemills930@gmail.com>
2022-09-19 18:51:04 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+/*
+ * File: 3-puts.c
+ *
+ * Auth: Mike Mills
+ */
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _puts - Prints a string to stdout
+ *
+ * @str: The string to be printed
+ */
+void _puts(char *str)
+{
+	while(*str)
+		putchar(*str++);
+	
+	putchar('\n');
+}
