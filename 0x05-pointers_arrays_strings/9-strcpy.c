0345eab8fae3848a90ce4c8885ffe2a5ed02291f
mikemills254 <mikemills930@gmail.com>
2022-09-20 18:31:55 -0700

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,8 +6,8 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * _strcpy - Copies a string pointed to by @src, including the
- * 	     terminating null byte, to a buffer pointed to by @dest.
+ * _strcpy - Copies a string pointed to by @src
+ *
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
  *
